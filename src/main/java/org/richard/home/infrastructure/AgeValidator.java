07ce11ce42e439d679337ac4b999e4699276fcfa
Richard Krasowski <richard.krasowski@mytoys.de>
2024-02-10 15:03:10 +0100

    small improvements

--- src/main/java/org/richard/home/infrastructure/AgeValidator.java
+++ src/main/java/org/richard/home/infrastructure/AgeValidator.java
@@ -11,12 +11,13 @@ import org.slf4j.LoggerFactory;
 @SupportedValidationTarget({ValidationTarget.PARAMETERS})
 public class AgeValidator implements ConstraintValidator<AgeMustBeAtLeastEighteen, Object[]> {
     private static Logger log = LoggerFactory.getLogger(AgeValidator.class);
+
     @Override
     public boolean isValid(Object[] value, ConstraintValidatorContext context) {
         log.info("validator called...");
-        if((Integer)value[0] + (Integer)value[1] < 18){
+        if ((Integer) value[0] + (Integer) value[1] < 18) {
             throw new IllegalArgumentException("age must be greater than 18");
-        };
+        }
         return true;
     }
 }
